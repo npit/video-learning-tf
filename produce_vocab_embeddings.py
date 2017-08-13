2e52a2b74496fd871d4b969f7ef881e02f5d5892
npit <pittarasnikif@gmail.com>
2017-08-13 21:53:36 +0300

    Fix captioning bad test data. Functionize captioning scripts

--- produce_vocab_embeddings.py
+++ produce_vocab_embeddings.py
@@ -6,75 +6,77 @@ from process_annotations import read_vocabulary
 '''
 Script to encode vocabulary contents into the embedding matrix. 
 '''
-init_file = "config.ini"
-# vocabulary and embeddings files
-vocabulary_file = None
-embeddings_file_type = "glove"
-embeddings_file = "/path/to/embeddings"
-randomize_missing_embeddings = False
 
-if len(sys.argv) > 1:
-    init_file = sys.argv[1]
-print("Using initialization file : ",init_file)
 
-keyvals = init_config(init_file, "captions")
+if __name__ == '__main__':
+    init_file = "config.ini"
+    # vocabulary and embeddings files
+    vocabulary_file = None
+    embeddings_file_type = "glove"
+    embeddings_file = "/path/to/embeddings"
+    randomize_missing_embeddings = False
 
-for key in keyvals:
-    exec("%s=%s" % (key, keyvals[key]))
-print("Successfully initialized from file %s" % init_file)
+    if len(sys.argv) > 1:
+        init_file = sys.argv[1]
+    print("Using initialization file : ", init_file)
 
-# read the vocabulary
-vocab = read_vocabulary(vocabulary_file)
+    keyvals = init_config(init_file, "captions")
 
-# switch by type of embeddings read
-if embeddings_file_type == "glove":
-    embeddings = {}
+    for key in keyvals:
+        exec("%s=%s" % (key, keyvals[key]))
+    print("Successfully initialized from file %s" % init_file)
 
-    # read the whole contents and compute the min/max in which to produce random embeddings for not found words
-    print("Reading embeddings file...")
-    with open(embeddings_file,"r") as fp:
-        for line in fp:
-            contents = line.split()
-            token = contents[0]
-            vector = list(map(float, contents[1:]))
-            embeddings[token] = vector
+    # read the vocabulary
+    vocab = read_vocabulary(vocabulary_file)
 
-    keys = list(embeddings.keys())
-    key = keys[0]
-    embedding_dim = len(embeddings[key])
+    # switch by type of embeddings read
+    if embeddings_file_type == "glove":
+        embeddings = {}
 
-    just_embeddings = [embeddings[w] for w in embeddings]
-    embedding_minmax = [np.amin(just_embeddings), np.amax(just_embeddings)]
-    print("Embedding dimension read :", embedding_dim, "\nDone.")
-    print("Embedding min/max read :", str(embedding_minmax))
+        # read the whole contents and compute the min/max in which to produce random embeddings for not found words
+        print("Reading embeddings file...")
+        with open(embeddings_file, "r") as fp:
+            for line in fp:
+                contents = line.split()
+                token = contents[0]
+                vector = list(map(float, contents[1:]))
+                embeddings[token] = vector
 
-    vocab_embeddings =  dict( (w, embeddings[w]) for w in [ w for w in vocab if w in embeddings])
-    missing_vocab_word_embeddings = [ w for w in vocab if w not in vocab_embeddings]
-    if len(missing_vocab_word_embeddings) > 3: # apart from EOS, BOS, UNK
-        print("%d items in the vocabulary were not found in the embedding matrix (other than EOS,BOS,UNK)!" %
-              (len(missing_vocab_word_embeddings) -3))
+        keys = list(embeddings.keys())
+        key = keys[0]
+        embedding_dim = len(embeddings[key])
 
+        just_embeddings = [embeddings[w] for w in embeddings]
+        embedding_minmax = [np.amin(just_embeddings), np.amax(just_embeddings)]
+        print("Embedding dimension read :", embedding_dim, "\nDone.")
+        print("Embedding min/max read :", str(embedding_minmax))
+
+        vocab_embeddings = dict((w, embeddings[w]) for w in [w for w in vocab if w in embeddings])
+        missing_vocab_word_embeddings = [w for w in vocab if w not in vocab_embeddings]
+        if len(missing_vocab_word_embeddings) > 3:  # apart from EOS, BOS, UNK
+            print("%d items in the vocabulary were not found in the embedding matrix (other than EOS,BOS,UNK)!" %
+                  (len(missing_vocab_word_embeddings) - 3))
+
+        else:
+            print("No missing embeddings other than EOS, BOS and UNK.")
+        if not randomize_missing_embeddings:
+            print("Randomizing embeddings is disabled, exiting. See missing_embeddings.txt")
+            with open("missing_embeddings.txt", "w") as f:
+                f.write("\n".join(missing_vocab_word_embeddings))
+            exit(1)
+
+        for w in missing_vocab_word_embeddings:
+            arr = np.random.uniform(low=embedding_minmax[0], high=embedding_minmax[1], size=(embedding_dim,))
+            print("Producing vector for missing token:", w)
+            vocab_embeddings[w] = arr
+
+        # write out
+        filename = vocabulary_file + ".embeddings"
+        print("Writing embeddings for vocabulary at", filename)
+
+        with open(filename, "w") as fp:
+            for token, vector in vocab_embeddings.items():
+                vec = " ".join(["%5.5f" % v for v in vector])
+                fp.write("%s\t%s\n" % (token, vec))
     else:
-        print("No missing embeddings other than EOS, BOS and UNK.")
-    if not randomize_missing_embeddings:
-        print("Randomizing embeddings is disabled, exiting. See missing_embeddings.txt")
-        with open("missing_embeddings.txt","w") as f:
-            f.write("\n".join(missing_vocab_word_embeddings))
-        exit(1)
-
-    for w in missing_vocab_word_embeddings:
-        arr = np.random.uniform(low=embedding_minmax[0], high=embedding_minmax[1], size=(embedding_dim,))
-        print("Producing vector for missing token:",w)
-        vocab_embeddings[w] = arr
-
-            
-    # write out
-    filename = vocabulary_file + ".embeddings"
-    print("Writing embeddings for vocabulary at",filename)
-
-    with open(filename,"w") as fp:
-        for token,vector in vocab_embeddings.items():
-            vec = " ".join([ "%5.5f" % v for v in vector])
-            fp.write("%s\t%s\n" % (token ,vec))
-else:
-    error("Unsupported embeddings file type: ", embeddings_file_type)
+        error("Unsupported embeddings file type: ", embeddings_file_type)
