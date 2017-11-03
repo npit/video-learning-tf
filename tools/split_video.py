d0c1ba6b7bb12bf9a88f7ba7736f1bd3d5c08c88
npit <pittarasnikif@gmail.com>
2017-11-04 01:22:55 +0200

    Update video splitter

--- tools/split_video.py
+++ tools/split_video.py
@@ -1,6 +1,9 @@
 import os, sys
 import subprocess
 
+if len(sys.argv) < 3:
+    print("Need videofile and shotsfile")
+    exit(1)
 fps=25
 extension = "avi"
 endtime="23:59:59"
@@ -12,8 +15,8 @@ outvidfolder = os.path.join(outfolder, vidname)
 if not os.path.exists(outfolder):
     os.makedirs(outvidfolder)
 
-def sec_from_nframes(nframes,nfps):
-    return nframes / nfps
+def sec_from_nframes(num, nfps):
+    return num / nfps
 
 def sec_to_hms(duration_sec):
     m, s = divmod(duration_sec, 60)
@@ -31,29 +34,29 @@ with open(shotfile,"r") as f:
 # calc time pairs
 timepairs = []
 curr_start = 0
-print("start-sec duration start_hms duration_hms")
+#print("start-sec duration start_hms duration_hms")
 for idx,frn in enumerate(framenums):
     start_hms = sec_to_hms(curr_start)
 
     duration = sec_from_nframes(frn-curr_start, fps)
     duration_hms = sec_to_hms(duration)
     timepairs.append((start_hms, duration_hms))
-    print(" %f %f " % (curr_start, duration))
+    #print(" %f %f " % (curr_start, duration))
     curr_start += duration
 
-print()
-print("resulting frame nums - timetags")
+#print()
+#print("resulting frame nums - timetags")
 timepairs.append((sec_to_hms(curr_start),endtime  ))
 framenums = [0] + framenums + [-1]
-for tp, frm in zip(timepairs, framenums):
-    print(frm, tp)
+#for tp, frm in zip(timepairs, framenums):
+#    print(frm, tp)
 # use -c copy for ffmpeg
 
 cmds = []
 for idx, tp in enumerate(timepairs):
     outfile = os.path.join(outvidfolder, "video_" + str(idx) + "." + extension)
     cmd=[]
-    cmd.extend('ffmpeg -loglevel warning -y -i'.split() + [vidfile] + "-c copy -ss".split() + [tp[0],"-t",tp[1],"-sn",outfile])
-    print(" ".join(cmd))
+    cmd.extend('ffmpeg -loglevel warning -y -i'.split() + [vidfile] + " -ss".split() + [tp[0],"-t",tp[1],"-sn",outfile])
+    #print(" ".join(cmd))
     process=subprocess.run(cmd)
 
