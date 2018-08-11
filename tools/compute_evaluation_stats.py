ec77405dc6b1edd23db76ead06a1678158cf3fab
npit <pittarasnikif@gmail.com>
2018-08-11 16:56:43 +0300

    Rework stats evaluator

--- tools/compute_evaluation_stats.py
+++ tools/compute_evaluation_stats.py
@@ -1,12 +1,42 @@
 from sklearn.metrics import f1_score, precision_score, recall_score
 import argparse
+import os
 import pickle
 import numpy
+import yaml
 
-path_dset="/run/media/nik/TOSHIBA EXT/Academic/msc-thesis/datasets/kth/serialized/val.txt"
-path_logits="/run/media/nik/TOSHIBA EXT/Academic/msc-thesis/runs/kth/singleframe/validation_logits_config.kth.vec.yml_val_resume_120618_175935.total"
+parser = argparse.ArgumentParser()
+parser.add_argument("logits_path")
 
+args = parser.parse_args()
+path_logits= args.logits_path
+print("Logits:", path_logits)
 
+# deduce dataset from the local config file
+path_dset = None
+run_folder = os.path.dirname(path_logits)
+config_file = [x for x in os.listdir(run_folder) if x.startswith("config") and x.endswith(".yml") and "graph-" not in x]
+assert len(config_file) == 1, "Multiple config files deduced: %s" % config_file
+config_file = os.path.join(run_folder, config_file[0])
+print("Deduced base config file:", config_file)
+
+with open(config_file, "r") as f:
+    data = yaml.load(f)["run"]["data"]
+    for datname in data:
+        print("Checking data path:", datname)
+        dat = data[datname]
+        if dat["phase"] != "defs.phase.val":
+            continue
+        if dat["tag"] != "defs.dataset_tag.main":
+            continue
+        if path_dset is not None:
+            print("Path already configured to", path_dset)
+        print("Deduced dataset identifier:", datname)
+        path_dset = dat["data_path"]
+
+print("Deduced data path:", path_dset)
+
+# read labels
 labels=[]
 logits=None
 with open(path_dset) as f:
@@ -15,11 +45,13 @@ with open(path_dset) as f:
         labels.append(lbl)
 
 with open(path_logits,"rb") as f:
-    logits=pickle.load(f)
+    logits = pickle.load(f)
 
-amax=numpy.argmax(logits, axis=1)
-print("Logits:", path_logits)
-print("F1 PREC REC")
-print(f1_score(labels, amax.tolist(),average='macro'))
-print(precision_score(labels, amax.tolist(),average='macro'))
-print(recall_score(labels, amax.tolist(),average='macro'))
+amax = numpy.argmax(logits, axis=1)
+
+print("Accuracy:")
+for avg in ["macro", "micro"]:
+    print(avg)
+    print("\tF1:", f1_score(labels, amax.tolist(),average=avg), sep="\t")
+    print("\tP:", precision_score(labels, amax.tolist(),average=avg), sep="\t")
+    print("\tR:", recall_score(labels, amax.tolist(),average=avg), sep="\t")
